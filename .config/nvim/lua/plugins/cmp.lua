47bf1808815d74ce6ca5a942461899ba0e7f4632
nuhakala <nuuttih@mailbox.org>
2025-04-10 10:06:45 +0300

    nvim update

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -2,10 +2,12 @@ return {
 	{
 		"saghen/blink.cmp",
 		version = "1.*",
+		dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
 
 		---@module 'blink.cmp'
 		---@type blink.cmp.Config
 		opts = {
+			snippets = { preset = "luasnip" },
 			keymap = { preset = "default" },
 			appearance = { nerd_font_variant = "mono" },
 			completion = { documentation = { auto_show = false } },
--- .config/nvim/lua/plugins/snippets.lua
+++ .config/nvim/lua/plugins/snippets.lua
@@ -1,34 +1,34 @@
 return {
-	-- {
-	-- 	"L3MON4D3/LuaSnip",
-	-- 	dependencies = {
-	-- 		-- { "rafamadriz/friendly-snippets" }, -- Snippet library
-	-- 	},
-	-- 	-- follow latest release.
-	-- 	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- 	event = "VeryLazy",
-	-- 	-- install jsregexp (optional!).
-	-- 	build = "make install_jsregexp",
-	-- 	config = function()
-	-- 		-- require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/luasnip/" } })
- --            require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/vsc_snippets/" } })
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			-- { "rafamadriz/friendly-snippets" }, -- Snippet library
+		},
+		-- follow latest release.
+		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+		event = "VeryLazy",
+		-- install jsregexp (optional!).
+		build = "make install_jsregexp",
+		config = function()
+			require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/luasnip/" } })
+            require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets/" } })
 
-	-- 		-- Load friendly-snippets
-	-- 		-- require("luasnip.loaders.from_vscode").lazy_load()
+			-- Load friendly-snippets
+			-- require("luasnip.loaders.from_vscode").lazy_load()
 
-	-- 		local luasnip = require("luasnip")
-	-- 		-- vim.keymap.set({"i"}, "<C-Space>", function() luasnip.expand() end, {silent = true})
-	-- 		vim.keymap.set({ "i", "s" }, "<C-L>", function()
-	-- 			luasnip.jump(1)
-	-- 		end, { silent = true })
-	-- 		vim.keymap.set({ "i", "s" }, "<C-K>", function()
-	-- 			luasnip.jump(-1)
-	-- 		end, { silent = true })
-	-- 		vim.keymap.set({ "i", "s" }, "<C-c>", function()
-	-- 			if luasnip.choice_active() then
-	-- 				luasnip.change_choice(1)
-	-- 			end
-	-- 		end, { silent = true })
-	-- 	end,
-	-- },
+			local luasnip = require("luasnip")
+			-- vim.keymap.set({"i"}, "<C-Space>", function() luasnip.expand() end, {silent = true})
+			vim.keymap.set({ "i", "s" }, "<C-L>", function()
+				luasnip.jump(1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-K>", function()
+				luasnip.jump(-1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-c>", function()
+				if luasnip.choice_active() then
+					luasnip.change_choice(1)
+				end
+			end, { silent = true })
+		end,
+	},
 }
